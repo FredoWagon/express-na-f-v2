fa44c16d5d12bace203ba957926469e171becf96
fredowagon <fedde.leg@gmail.com>
2021-07-20 18:08:43 +0200

    pour sami

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+const { sendMessage } = require('../lib/nodemailer')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+router.get('/bio', function (req, res, next) {
+  res.render('bio');
+});
+
+router.get('/contact', function (req, res, next) {
+
+  res.render('contact')
+})
+
+router.post('/contact', function (req, res, next) {
+  console.log(req.body)
+  sendMessage();
+})
 
 module.exports = router;
